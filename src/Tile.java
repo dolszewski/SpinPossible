e469985ebc2ac1c21160270a2ad1a08a84ebb87a
Daniel Olszewski <dolszewski@carroll.edu>
2018-09-26 15:47:10 -0600

    Added basic constructor, class variables, and getters/setters

--- src/Tile.java
+++ src/Tile.java
@@ -1,4 +1,24 @@
 
 public class Tile {
-
+	private static int currentNumber = 0;
+	private String spin;
+	private int value;
+	public Tile() {
+		currentNumber++;
+		this.value = currentNumber;
+		this.spin = "UP";
+	}
+	public String getSpin() {
+		return spin;
+	}
+	public void setSpin(String spin) {
+		this.spin = spin;
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
 }
