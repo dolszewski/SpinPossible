f7772f43bad1ebb612a42bb85ee47f831486a4ab
Terry Cox <tcox@carroll.edu>
2018-10-17 15:21:29 -0600

    Highlighted and Selected functions

--- src/SpinController.java
+++ src/SpinController.java
@@ -5,20 +5,24 @@ import java.util.TimerTask;
 
 import javax.swing.JFrame;
 
+import stack.ArrayStack;
+import stack.StackInterface;
+
 class SpinController extends TimerTask implements MouseListener, SpinControllerInterface{
     private JFrame gameJFrame;
     private Container gameContentPane;
     private boolean gameIsReady = false;
+    private StackInterface<Integer[]> spinStack = new ArrayStack<>();
+    private Board theBoard = new Board();
+    private int[][] selectedArray = new int[2][2];
+    private int numberSelected = 0;
+    
     
 
 	public static void main(String[] args) {
-		Board b = new Board();
-		System.out.print(b.toString());
-	}
-	
-	public void spin() {
 		
 	}
+	
 
 	@Override
 	public void run() {
@@ -55,4 +59,84 @@ class SpinController extends TimerTask implements MouseListener, SpinControllerI
 		// TODO Auto-generated method stub
 		
 	}
+
+
+	@Override
+	public void spin(int first, int second) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	@Override
+	public void highlightTiles() {
+		// TODO Auto-generated method stub
+		if(numberSelected == 2) {
+			int minA = min(selectedArray[0][0], selectedArray[1][0]);
+			int maxA = max(selectedArray[0][0], selectedArray[1][0]);
+			int minB = min(selectedArray[0][1], selectedArray[1][1]);
+			int maxB = max(selectedArray[0][1], selectedArray[1][1]);
+			
+			for(int i = minA; i < maxA+1; i++) {
+				for(int j = minB; j < maxB+1; j++) {
+					theBoard.getBoard()[i][j].setHighlighted(true);
+				}
+			}
+		}
+		else if(numberSelected == 1) {
+			theBoard.getBoard()[selectedArray[0][0]][selectedArray[1][0]].setHighlighted(true);
+		}
+
+	}
+	
+	public int min(int a, int b) {
+		if(a < b)
+			return a;
+		else
+			return b;
+	}
+	
+	public int max(int a, int b) {
+		if(a > b)
+			return a;
+		else
+			return b;
+	}
+
+
+	@Override
+	public void selected(int row, int column) {
+		// TODO Auto-generated method stub
+		if(numberSelected == 0) { 
+			selectedArray[0][0] = row;
+			selectedArray[0][1] = column;
+			numberSelected++;
+		}
+		else if(numberSelected == 1) {
+			if(selectedArray[0][0] == row && selectedArray[0][1] == column)
+				numberSelected--;
+			else {
+			selectedArray[1][0] = row;
+			selectedArray[1][1] = column;
+			numberSelected++;
+			}
+		}
+		else if(numberSelected == 2) {
+			if(selectedArray[1][0] == row && selectedArray[1][1] == column)
+				numberSelected--;
+			else if(selectedArray[0][0] == row && selectedArray[0][1] == column) {
+				numberSelected--;
+				selectedArray[0][0] = selectedArray[1][0];
+				selectedArray[0][1] = selectedArray[1][1];
+			}
+			else {
+				selectedArray[1][0] = row;
+				selectedArray[1][1] = column;
+			}
+		}
+	}
+
+	
+
+
 }
